1e862ccf4ed7edb8872c245e44646e8434348604
yunheewon <yunheewon66@gmail.com>
2025-08-16 19:03:11 +0900

    feature/#3: 동/민원종류 카테고리별 검색, 날짜 필터, 제목내용 키워드 검색 기능 구현

--- wetown/urls.py
+++ wetown/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView #추가
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),  
