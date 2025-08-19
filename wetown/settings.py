eaa4bfa4889fd4f40bffb3744d13e1d0b4f9cf72
yunheewon <yunheewon66@gmail.com>
2025-08-19 17:14:03 +0900

    프론트연동설정 수정2

--- wetown/settings.py
+++ wetown/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
