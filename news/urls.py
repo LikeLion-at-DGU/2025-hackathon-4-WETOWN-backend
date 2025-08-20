a0e96584a3175b3a57093041fc6ff71731de6894
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 15:25:00 +0900

    news/urls.py 라우터 구현

--- news/urls.py
+++ news/urls.py
@@ -1,3 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import NewsViewSet
 
-urlpatterns = []
+app_name = "news"
+
+default_router = routers.SimpleRouter(trailing_slash=False)
+default_router.register("news", NewsViewSet, basename="news")
+
+urlpatterns = [
+  path("", include(default_router.urls)),  
+]
