3e3006cef9904dc174ea71a9fac41ed7b5d782e0
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 20:22:48 +0900

    posts urls.py 2차 수정

--- posts/urls.py
+++ posts/urls.py
@@ -19,5 +19,5 @@ post_comment_router.register("comments", PostCommentViewSet, basename="post-comm
 urlpatterns = [
     path("", include(default_router.urls)),
     path("", include(comment_router.urls)),
-    path("posts/<int:post_id>/", include(post_comment_router.urls)),
+    path("<int:post_id>/", include(post_comment_router.urls)),
 ]
