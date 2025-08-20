58597b2fb52ed82ca04ec386925cf321c25f3930
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 20:20:54 +0900

    posts 앱 urls.py 중복 post 라우팅 제거

--- posts/urls.py
+++ posts/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 app_name = "posts"
 
 default_router = routers.SimpleRouter(trailing_slash=False)
-default_router.register("posts", PostViewSet, basename="posts")
+default_router.register("", PostViewSet, basename="posts")
 
 comment_router = routers.SimpleRouter(trailing_slash=False)
 comment_router.register("comments", CommentViewSet, basename="comment")  #전체 댓글 API 
