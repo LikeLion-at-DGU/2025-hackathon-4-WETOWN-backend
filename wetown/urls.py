1eb94ef963c6c1e5b070ae96ef286149c75e4292
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 11:13:08 +0900

    news, posts 앱 기본 세팅

--- wetown/urls.py
+++ wetown/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.views.generic import TemplateView #추가
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),  
-]
-
-
+    path('posts/', include('posts.urls')),
+    path('surveys/', include('surveys.urls')),
+    path('news/', include('news.urls')),
+    # path('', include('posts.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
