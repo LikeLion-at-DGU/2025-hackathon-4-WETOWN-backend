c8267705fffcaee5b925e278349a08850a4d19d4
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 21:21:22 +0900

    news/surveys urls.py 라우팅 수정

--- surveys/urls.py
+++ surveys/urls.py
@@ -5,7 +5,7 @@ from .views import SurveyViewSet
 app_name = "surveys"
 
 default_router = routers.SimpleRouter(trailing_slash=False)
-default_router.register("surveys", SurveyViewSet, basename="surveys")
+default_router.register("", SurveyViewSet, basename="surveys")
 
 urlpatterns = [
   path("", include(default_router.urls)),
