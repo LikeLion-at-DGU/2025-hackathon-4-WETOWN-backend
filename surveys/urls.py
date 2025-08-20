7941033255ead3c863527be1378aa92851fe1fdb
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 17:58:12 +0900

    Surveys urls.py 추가 (SimpleRouter 경로 라우팅)

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,3 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import SurveyViewSet
 
-urlpatterns = []
+app_name = "surveys"
+
+default_router = routers.SimpleRouter(trailing_slash=False)
+default_router.register("surveys", SurveyViewSet, basename="surveys")
+
+urlpatterns = [
+  path("", include(default_router.urls)),
+]
