95f4baa6a9a82952dfec2c2c4f1f3d2ea9337f4f
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-20 13:56:18 +0900

    News 모델 models.py 추가

--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+def news_image_upload_path(instance,filename):
+  return f"news/{instance.pk}/{filename}"
+
+class News(models.Model): # 뉴스 모델 임시 설계 (차후 ai 도입 시 변경)
+  title = models.CharField(max_length=150, db_index=True)
+  body = models.TextField(blank=True)
+  image = models.ImageField(upload_to=news_image_upload_path, blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ["-created_at"]
+    indexes = [models.Index(fields=["-created_at"])]
+
+  def __str__(self):
+    return f"[News {self.id}] (self.title)"
