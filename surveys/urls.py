095200defe5200d2776bdb3c7317a5c0a664d916
junh5ng <wnsghd1130@dgu.ac.kr>
2025-08-21 16:54:35 +0900

    Surveys urls.py 코드 검증 엔드포인트 추가

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import SurveyViewSet
+from .views import SurveyViewSet, VerifySurveyCodeView
 
 app_name = "surveys"
 
@@ -8,5 +8,6 @@ default_router = routers.SimpleRouter(trailing_slash=False)
 default_router.register("", SurveyViewSet, basename="surveys")
 
 urlpatterns = [
+  path("verify-code", VerifySurveyCodeView.as_view(), name="surveys-verify-code"),
   path("", include(default_router.urls)),
 ]
